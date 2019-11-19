e6d2b8c2ed26b90e9d3c8a2c75d60aac0a1d3500
Felipe <felipetirus@gmail.com>
2019-11-19 20:12:00 +0000

    Updates

---
+++ precache-manifest.e328b46d2206139aa1e8cd40aed14789.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4593f2c6b86a5d2b238a082cb69cec99",
+    "url": "/super-list/index.html"
+  },
+  {
+    "revision": "ae3c62594c4198f0953c",
+    "url": "/super-list/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f8f5585b2149c3c7361a",
+    "url": "/super-list/static/js/2.af334a2d.chunk.js"
+  },
+  {
+    "revision": "ae3c62594c4198f0953c",
+    "url": "/super-list/static/js/main.b59cc334.chunk.js"
+  },
+  {
+    "revision": "6e8a2459c40f3c645b3c",
+    "url": "/super-list/static/js/runtime-main.a1e14e54.js"
+  }
+]);
